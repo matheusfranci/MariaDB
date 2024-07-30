24d18f8099d6b95956c4fe55f50e2fd0e8387efa
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-07-29 22:25:25 -0300

    Update Tuning_Restore.sql

--- Tuning_Restore.sql
+++ Tuning_Restore.sql
@@ -1,21 +1,4 @@
--- Verificando os parâmetros de verificação de consistência:
-SHOW VARIABLES LIKE 'foreign_key_checks';
-SHOW VARIABLES LIKE 'unique_checks';
-
--- Alterando temporariamente para a sessão atual:
-SET foreign_key_checks = 0;
-SET unique_checks = 0;
-
--- Alterando definitivamente para todas as sessões:
--- edit o my.cnf
-[mysqld]
-foreign_key_checks = 0
-unique_checks = 0
-
--- Depois de alterar reinicie o mariadb para persistir
-sudo systemctl restart mariadb
-
--- Também é pertinente tunar os parâmetros innodb_buffer_pool_size e o innodb_log_file_size
+-- É pertinente tunar os parâmetros innodb_buffer_pool_size e o innodb_log_file_size
 SHOW VARIABLES LIKE 'innodb_buffer_pool_size';
 SHOW VARIABLES LIKE 'innodb_log_file_size';
 
